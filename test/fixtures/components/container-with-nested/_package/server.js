548f0a1fef038f4dfded3d2302687c44b6f12973
Nick Balestra <nbalestra@opentable.com>
2017-07-12 16:52:19 +0100

    unprettified packaged files

--- test/fixtures/components/container-with-nested/_package/server.js
+++ test/fixtures/components/container-with-nested/_package/server.js
@@ -1,6 +1,1 @@
-'use strict';
-module.exports.data = function(n, e) {
-  n.renderComponent('no-containers', function(n, t) {
-    e(n, { nested: t });
-  });
-};
+"use strict";module.exports.data=function(n,e){n.renderComponent("no-containers",function(n,t){e(n,{nested:t})})};
