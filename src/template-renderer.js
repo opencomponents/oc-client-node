9620bb8642b1f8a6d0b323ae74028931995c4be0
Matteo Figus <matteofigus@gmail.com>
2017-11-07 14:11:04 +0000

    Destructuring

--- src/template-renderer.js
+++ src/template-renderer.js
@@ -3,19 +3,19 @@
 const htmlRenderer = require('./html-renderer');
 const requireTemplate = require('./utils/require-template');
 
+const isTemplateLegacy = t => !!{ handlebars: true, jade: true }[t];
+
 module.exports = function() {
   return function(template, model, options, callback) {
-    const key = { options };
-    let type = options.templateType;
-    if (type === 'jade') {
-      type = 'oc-template-jade';
-    }
-    if (type === 'handlebars') {
-      type = 'oc-template-handlebars';
+    const { key } = options;
+    let { templateType } = options;
+
+    if (isTemplateLegacy(templateType)) {
+      templateType = `oc-template-${templateType}`;
     }
 
     try {
-      const ocTemplate = requireTemplate(type);
+      const ocTemplate = requireTemplate(templateType);
       ocTemplate.render({ key, model, template }, (err, html) => {
         options.html = html;
         return callback(err, htmlRenderer.renderedComponent(options));
